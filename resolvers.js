83d6612754dda1fd8210ddc15fb11b521bccccfc
Anik Mohammad <anik7303@gmail.com>
2023-08-09 18:24:55 +0600

    (server) add graphql mutation to update job info

--- resolvers.js
+++ resolvers.js
@@ -1,5 +1,5 @@
 import { getCompany } from "./db/companies.js";
-import { createJob, getJob, getJobs, getJobsByCompany } from "./db/jobs.js";
+import { createJob, getJob, getJobs, getJobsByCompany, updateJob } from "./db/jobs.js";
 import { toIsoDate } from "./lib/utils.js";
 
 export const resolvers = {
@@ -12,6 +12,7 @@ export const resolvers = {
   Mutation: {
     createJob: (_, { input: { title, description } }, { user }) =>
       createJob({ companyId: user.companyId, title, description }),
+    updateJob: (_, { input: { id, title, description } }) => updateJob({ id, title, description }),
   },
 
   Job: {
