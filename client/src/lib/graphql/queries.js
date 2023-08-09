c32b0f57d5afa8874ef1efc3d6c526f7f3b1bb77
Anik Mohammad <anik7303@gmail.com>
2023-08-09 16:51:37 +0600

    (client) refactor HomePage to fetch data from graphql server

--- client/src/lib/graphql/queries.js
+++ client/src/lib/graphql/queries.js
@@ -4,3 +4,21 @@ import { GraphQLClient } from "graphql-request";
 const url = "http://localhost:9000/graphql";
 
 const client = new GraphQLClient(url);
+
+export async function getJobs() {
+  const query = `#graphql
+    query Jobs {
+      jobs {
+        id
+        title
+        date
+        company {
+          id
+          name
+        }
+      }
+    }
+  `;
+  const { jobs } = await client.request(query);
+  return jobs;
+}
