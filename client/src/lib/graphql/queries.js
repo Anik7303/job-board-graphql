9128df9fd7755cb0a360a1e85ce66180cdb76bbd
Anik Mohammad <anik7303@gmail.com>
2023-08-09 22:32:33 +0600

    (client) add pagination

--- client/src/lib/graphql/queries.js
+++ client/src/lib/graphql/queries.js
@@ -45,6 +45,7 @@ export const jobsQuery = gql`
           name
         }
       }
+      total
     }
   }
 `;
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,8 +1,13 @@
+import { useState } from "react";
 import JobList from "../components/JobList";
+import PaginationBar from "../components/PaginationBar";
 import { useJobs } from "../lib/graphql/hooks";
 
 function HomePage() {
-  const { data, loading, error } = useJobs();
+  const perPage = 5;
+  const [currentPage, setCurrentPage] = useState(1);
+  const { data, loading, error } = useJobs(perPage, (currentPage - 1) * perPage);
+  const totalPages = Math.ceil(data?.total / perPage);
 
   if (loading) return <div>Loading...</div>;
   if (error) return <div className="has-text-danger">Data unavailable</div>;
@@ -10,6 +15,7 @@ function HomePage() {
     <div>
       <h1 className="title">Job Board</h1>
       <JobList jobs={data?.items} />
+      <PaginationBar currentPage={currentPage} totalPages={totalPages} onPageChange={setCurrentPage} />
     </div>
   );
 }
